ad69cae2982486f8ae9b9e8aaca0098c931ebdd9
Ilya Miller <yax@mail.com>
2025-02-25 15:41:53 +0300

    lang glitch fix

--- system.py
+++ system.py
@@ -1,10 +1,22 @@
 import ctypes
+import ctypes
+import collections
 
-langs = {68748313: "ru",67699721: "en"}
+langs = {68748313: "ru", 67699721: "en"}
+buffer_size = 5
+lang_buffer = collections.deque(maxlen=buffer_size)
+
 
 def get_lang():
     user32 = ctypes.windll.user32
     hwnd = user32.GetForegroundWindow()
     thread = user32.GetWindowThreadProcessId(hwnd, None)
     layout = user32.GetKeyboardLayout(thread)
-    return langs.get(layout)
+    lang = langs.get(layout)
+
+    if lang:
+        lang_buffer.append(lang)
+
+    if len(lang_buffer) == buffer_size:
+        return max(set(lang_buffer), key=lang_buffer.count)
+    return lang
